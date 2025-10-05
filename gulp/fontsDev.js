43d696fa56034253c7d9f7208f252035e27cdf09
bahodir-media <bahodir.media@gmail.com>
2025-10-05 22:53:51 +0500

    Fonts are added

--- gulp/fontsDev.js
+++ gulp/fontsDev.js
@@ -1,6 +1,5 @@
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
-
 const fs = require('fs');
 const gulp = require('gulp');
 const fonter = require('gulp-fonter-fix');
@@ -9,118 +8,115 @@ const ttf2woff2 = require('gulp-ttf2woff2');
 const srcFolder = './src';
 const destFolder = './build';
 
+// Convert .otf → .ttf
 gulp.task('otfToTtf', () => {
-	// Ищем файлы шрифтов .otf
-	return (
-		gulp
-			.src(`${srcFolder}/fonts/*.otf`, {})
-			// Конвертируем в .ttf
-			.pipe(
-				fonter({
-					formats: ['ttf'],
+	return gulp
+		.src(`${srcFolder}/fonts/**/*.otf`, {})
+		.pipe(
+			fonter({
+				formats: ['ttf'],
+			})
+		)
+		.pipe(gulp.dest(`${srcFolder}/fonts/`))
+		.pipe(
+			plumber(
+				notify.onError({
+					title: 'FONTS',
+					message: 'Error: <%= error.message %>. File: <%= file.relative %>!',
 				})
 			)
-			// Выгружаем в исходную папку
-			.pipe(gulp.dest(`${srcFolder}/fonts/`))
-			.pipe(
-				plumber(
-					notify.onError({
-						title: 'FONTS',
-						message:
-							'Error: <%= error.message %>. File: <%= file.relative %>!',
-					})
-				)
-			)
-	);
+		);
 });
 
+// Convert .ttf → .woff & .woff2 (with subfolder support)
 gulp.task('ttfToWoff', () => {
-	// Ищем файлы шрифтов .ttf
-	return (
-		gulp
-			.src(`${srcFolder}/fonts/*.ttf`, {})
-			// Конвертируем в .woff
-			.pipe(
-				fonter({
-					formats: ['woff'],
+	return gulp
+		.src(`${srcFolder}/fonts/**/*.ttf`, { base: `${srcFolder}/fonts/` })
+		// Convert to .woff
+		.pipe(
+			fonter({
+				formats: ['woff'],
+			})
+		)
+		.pipe(gulp.dest(`${destFolder}/fonts/`))
+		// Convert to .woff2
+		.pipe(gulp.src(`${srcFolder}/fonts/**/*.ttf`, { base: `${srcFolder}/fonts/` }))
+		.pipe(ttf2woff2())
+		.pipe(gulp.dest(`${destFolder}/fonts/`))
+		.pipe(
+			plumber(
+				notify.onError({
+					title: 'FONTS',
+					message: 'Error: <%= error.message %>',
 				})
 			)
-			// Выгружаем в папку с результатом
-			.pipe(gulp.dest(`${destFolder}/fonts/`))
-			// Ищем файлы шрифтов .ttf
-			.pipe(gulp.src(`${srcFolder}/fonts/*.ttf`))
-			// Конвертируем в .woff2
-			.pipe(ttf2woff2())
-			// Выгружаем в папку с результатом
-			.pipe(gulp.dest(`${destFolder}/fonts/`))
-			.pipe(
-				plumber(
-					notify.onError({
-						title: 'FONTS',
-						message: 'Error: <%= error.message %>',
-					})
-				)
-			)
-	);
+		);
 });
 
-gulp.task('fontsStyle', () => {
-	// Файл стилей подключения шрифтов
+// Auto-generate SCSS font includes
+gulp.task('fontsStyle', (done) => {
 	let fontsFile = `${srcFolder}/scss/base/_fontsAutoGen.scss`;
-	// Проверяем существуют ли файлы шрифтов
-	fs.readdir(`${destFolder}/fonts/`, function (err, fontsFiles) {
-		if (fontsFiles) {
-			// Проверяем существует ли файл стилей для подключения шрифтов
 
-				// Если файла нет, создаем его
-				fs.writeFile(fontsFile, '', cb);
-				let newFileOnly;
-				for (var i = 0; i < fontsFiles.length; i++) {
-					// Записываем подключения шрифтов в файл стилей
-					let fontFileName = fontsFiles[i].split('.')[0];
-					if (newFileOnly !== fontFileName) {
-						let fontName = fontFileName.split('-')[0]
-							? fontFileName.split('-')[0]
-							: fontFileName;
-						let fontWeight = fontFileName.split('-')[1]
-							? fontFileName.split('-')[1]
-							: fontFileName;
-						if (fontWeight.toLowerCase() === 'thin') {
-							fontWeight = 100;
-						} else if (fontWeight.toLowerCase() === 'extralight') {
-							fontWeight = 200;
-						} else if (fontWeight.toLowerCase() === 'light') {
-							fontWeight = 300;
-						} else if (fontWeight.toLowerCase() === 'medium') {
-							fontWeight = 500;
-						} else if (fontWeight.toLowerCase() === 'semibold') {
-							fontWeight = 600;
-						} else if (fontWeight.toLowerCase() === 'bold') {
-							fontWeight = 700;
-						} else if (
-							fontWeight.toLowerCase() === 'extrabold' ||
-							fontWeight.toLowerCase() === 'heavy'
-						) {
-							fontWeight = 800;
-						} else if (fontWeight.toLowerCase() === 'black') {
-							fontWeight = 900;
-						} else {
-							fontWeight = 400;
-						}
-						fs.appendFile(
-							fontsFile,
-							`@font-face {\n\tfont-family: ${fontName};\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`,
-							cb
-						);
-						newFileOnly = fontFileName;
-					}
+	fs.readdir(`${destFolder}/fonts/`, function (err, fontsFolders) {
+		if (!fontsFolders) return done();
+
+		// Recursive read helper
+		function readFonts(dir) {
+			let files = [];
+			fs.readdirSync(dir).forEach((item) => {
+				let fullPath = `${dir}/${item}`;
+				if (fs.lstatSync(fullPath).isDirectory()) {
+					files = files.concat(readFonts(fullPath));
+				} else if (/\.(woff2|woff)$/i.test(item)) {
+					files.push(fullPath.replace(`${destFolder}/fonts/`, ''));
 				}
-
+			});
+			return files;
 		}
-	});
 
-	return gulp.src(`${srcFolder}`);
-	function cb() {}
+		let allFonts = readFonts(`${destFolder}/fonts`);
+		fs.writeFileSync(fontsFile, '');
+
+		let addedFonts = new Set();
+
+		allFonts.forEach((fontPath) => {
+			let fontFileName = fontPath.split('/').pop().split('.')[0];
+			if (addedFonts.has(fontFileName)) return;
+			addedFonts.add(fontFileName);
+
+			let fontName = fontFileName.split('-')[0] || fontFileName;
+			let fontWeight = fontFileName.split('-')[1] || 'regular';
+
+			const weightMap = {
+				thin: 100,
+				extralight: 200,
+				light: 300,
+				regular: 400,
+				medium: 500,
+				semibold: 600,
+				bold: 700,
+				extrabold: 800,
+				heavy: 800,
+				black: 900,
+			};
+
+			fontWeight = weightMap[fontWeight.toLowerCase()] || 400;
+
+			fs.appendFileSync(
+				fontsFile,
+				`@font-face {\n\tfont-family: '${fontName}';\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontPath.replace(
+					/\\/g,
+					'/'
+				)}.woff2") format("woff2"), url("../fonts/${fontPath.replace(
+					/\\/g,
+					'/'
+				)}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\n\n`
+			);
+		});
+
+		done();
+	});
 });
 
+// Combined font build task
 gulp.task('fontsDev', gulp.series('otfToTtf', 'ttfToWoff', 'fontsStyle'));
